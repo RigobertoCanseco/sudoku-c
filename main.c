25c701ff7c85839ce59b8b31ec3dcd1112e081b4
Rigoberto Caneco <cansecorigoberto@gmail.com>
2021-01-27 18:39:17 -0600

    add new code

---
+++ main.c
@@ -0,0 +1,7 @@
+/**
+ * A mystery hello world in C
+ */
+#include <stdio.h>
+int main() {
+    printf("hello world");
+}
---
+++ src/sudoku.h
@@ -0,0 +1,10 @@
+//
+// Created by rigoberto on 27/01/21.
+//
+
+#ifndef SUDOKU_C_SUDOKU_H
+#define SUDOKU_C_SUDOKU_H
+int open_file() {
+
+}
+#endif //SUDOKU_C_SUDOKU_H
